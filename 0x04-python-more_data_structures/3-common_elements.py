bd06e4cd752e936c851ddb3c51fe7766fad4035f
josegit94 <joserevolver50@gmail.com>
2022-06-09 10:49:59 -0700

    fix

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# 3-common_element
+# 3-common_elements.py
 
-def common_element(set_1, set_2):
+def common_elements(set_1, set_2):
     """Return a set of common elements from 2 sets"""
     return (set_1 & set_2)
