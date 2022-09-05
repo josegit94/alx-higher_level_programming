8dcd8f3ccd3adf7cc9eed2e444c3ecc32b41a7db
josegit94 <joserevolver50@gmail.com>
2022-09-05 23:56:47 +0000

    fix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,6 +3,6 @@
 * this prints three lines
 */
 
-console.log("C is fun");
-console.log("Python is cool");
-console.log("Javascript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
