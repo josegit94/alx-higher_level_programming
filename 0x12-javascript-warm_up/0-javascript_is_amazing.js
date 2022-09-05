578f299ee9b4a8067aa2a12dd03033895c3b0c7e
josegit94 <joserevolver50@gmail.com>
2022-09-05 23:32:45 +0000

    fix

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-
 /**
 * Prints the constant variable
 */
-
 const myVar;
-	{ myVar = "Javascript is amazing";
-	console.log(myVar);}
+myVar = "Javascript is amazing";
+console.log(myVar);
